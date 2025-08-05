80213716e00ddeb016cca3d9b9eed634bb3e09f5
rog <you@example.com>
2025-08-05 10:59:10 +0200

    a

--- test.py
+++ test.py
@@ -3,11 +3,13 @@ sys.path.append('../galatae-api/')
 from robot import Robot
 
 def main():
-  r=Robot('/dev/ttyACM0')
-  r.debug=True
+  r=Robot('/dev/ttyACM0',True)
+  r.set_joint_speed(10)
+
   r.probe([300,0,300,90,0,0])
   print(r.get_tool_pose())
-  r.linear_move_to_point([0,300,300,90,0,0])
+  print(r.get_angles())
+  #r.linear_move_to_point([0,300,300,90,0,0])
 
 if __name__ == "__main__":
   main()
