582c261848b9ba6622c275421dd27a75ae534683
roger <roger@galatae.com>
2025-08-05 11:39:10 +0200

    a

--- test.py
+++ test.py
@@ -3,13 +3,12 @@ sys.path.append('../galatae-api/')
 from robot import Robot
 
 def main():
-  r=Robot('/dev/ttyACM0',True)
+  r=Robot('/dev/ttyACM0',0)
   r.set_joint_speed(10)
-
-  r.probe([300,0,300,90,0,0])
-  print(r.get_tool_pose())
-  print(r.get_angles())
-  #r.linear_move_to_point([0,300,300,90,0,0])
+  r.linear_move_to_point([200,0,300,90,0,0])
+  success=r.linear_probe([500,0,300,90,0,0])
+  print(success)
+  r.go_to_foetus_pos()
 
 if __name__ == "__main__":
   main()
